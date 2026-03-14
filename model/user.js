5acafb2f3baf4b41d2b93738a3cf9383488a704a
bibekshah220 <bibekshah425@gmail.com>
2026-03-15 01:23:30 +0545

    refactor user model to include schema definition and password hashing

--- model/user.js
+++ model/user.js
@@ -1,2 +1,48 @@
-import mangoose from 'mongoose';
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please add a name"],
+        trim: true,
+        maxlength: [25, "Name cannot be more than 25 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please add an email"],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please add a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],  
+        minlength: [8, "Password must be at least 8 characters"],
+        select: false
+    }
+}, {
+    timestamps: true
+});
+
+// Hash the password before saving in db
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    const salt = await bcryptjs.genSalt(10);
+    this.password = await bcryptjs.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcryptjs.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
