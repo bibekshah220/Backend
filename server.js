9d8dfab5a01790b63b8407278c227a894a25f4a2
bibekshah220 <bibekshah425@gmail.com>
2026-03-14 23:21:31 +0545

    add route to respond with 'Hello World!' on root path

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})  
+
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 })
