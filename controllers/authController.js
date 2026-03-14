949d4628f19c9d685e85171201d95a0f94dd7d02
bibekshah220 <bibekshah425@gmail.com>
2026-03-15 01:55:37 +0545

    implement user registration and login functionality with JWT authentication

---
+++ controllers/authController.js
@@ -0,0 +1,68 @@
+
+import User from "../model/user.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    });
+};
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body; 
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ success: false, error: "User already exists with this email" });
+        }  
+        
+        // Create new user
+        const user = await User.create({ name, email, password });
+
+        // Generate new token
+        const token = generateToken(user._id);
+        res.status(201).json({
+            success: true,
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email                   
+            },
+            token,
+            message: "User registered successfully"
+        }); 
+    } catch (error) {
+        res.status(500).json({ success: false, error: "Server Error" });
+    }   
+};
+
+// Login user
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select("+password");
+        if (!user) {
+            return res.status(400).json({ success: false, error: "Invalid email or password" });
+        }   
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
+            return res.status(400).json({ success: false, error: "Invalid email or password" });
+        }       
+        // Generate token
+        const token = generateToken(user._id);
+        res.status(200).json({  
+            success: true,  
+            data: {
+                _id: user._id,
+                name: user.name,
+                email: user.email   
+            },  
+            token,
+            message: "User logged in successfully"
+        }); 
+    } catch (error) {
+        res.status(500).json({ success: false, error: "Server Error" });
+    }           
+};
+
+export default { registerUser, loginUser };     
--- model/user.js
+++ model/user.js
@@ -34,13 +34,13 @@ userSchema.pre("save", async function(next) {
     if (!this.isModified("password")) {
         return next();
     }
-    const salt = await bcryptjs.genSalt(10);
-    this.password = await bcryptjs.hash(this.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
     next();
 });
 
 userSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcryptjs.compare(enteredPassword, this.password);
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
