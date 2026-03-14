d323a48c39ec6a79091b2bfe372d36dce779c43b
bibekshah220 <bibekshah425@gmail.com>
2026-03-14 23:26:38 +0545

    add contact route to respond with 'Contact Page'

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/about', (req, res) => {
     res.send('About Page')
 })  
 
+app.get("/contact", (req, res) => {
+    res.send('Contact Page')
+})
+
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 })
